894ce0ec222e0585ab71639946fdcf4029f07d1d
Marie Legrand <marie.legrand.dev@gmail.com>
2024-10-22 16:29:16 +0200

    Initial commit

---
+++ GLP/OpenGL-Starter/SDL-0/Main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <SDL.h>
+#include <glew.h>
+using namespace std;
+//#define GLEW_STATIC
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
+	}
+	else
+	{
+		cout << "SDL initialization succeeded!\n";
+	}
+	///////////SETTING UP SDL/////////////
+	//Create a simple window
+	int width = 400;
+	int height = 300;
+	unsigned int center = SDL_WINDOWPOS_CENTERED;
+	SDL_Window* Window = SDL_CreateWindow("My window", center, center, width, height, SDL_WINDOW_OPENGL);
+	//SDL_WINDOW_OPENGL is a u32 flag !
+
+
+	//Create an OpenGL compatible context to let glew draw on it
+	SDL_GLContext Context = SDL_GL_CreateContext(Window);
+
+	/////////SETTING UP OPENGL WITH GLEW///
+	//Initialize glew
+	glewExperimental = GL_TRUE;
+	if (glewInit() == GLEW_OK) {
+		cout << "Glew initialized successfully\n";
+	}
+
+	cin.get();
+	return 0;
+}
