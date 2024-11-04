71875feeb52b1c1b4f5c70017567bbcf510432eb
MaxSergueev <max.sergueev@artfx.fr>
2024-11-04 16:20:48 +0100

    ADD - Drawing triangle and couple simple shaders
    Now to add vertex colors

--- GLP/OpenGL-Starter/SDL-0/Main.cpp
+++ GLP/OpenGL-Starter/SDL-0/Main.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
 #include <SDL.h>
 #include <glew.h>
+#include <string>
+#include <fstream>
 using namespace std;
 //#define GLEW_STATIC
+string LoadShader(string fileName);
 
 int main(int argc, char* argv[])
 {
@@ -16,8 +19,8 @@ int main(int argc, char* argv[])
 	}
 	///////////SETTING UP SDL/////////////
 	//Create a simple window
-	int width = 400;
-	int height = 300;
+	int width = 500;
+	int height = 500;
 	unsigned int center = SDL_WINDOWPOS_CENTERED;
 	SDL_Window* Window = SDL_CreateWindow("My window", center, center, width, height, SDL_WINDOW_OPENGL);
 	//SDL_WINDOW_OPENGL is a u32 flag !
@@ -33,6 +36,119 @@ int main(int argc, char* argv[])
 		cout << "Glew initialized successfully\n";
 	}
 
+	//Set the viewing frame through which we will see the objects
+	glViewport(0, 0, width, height);
+
+	//Put the color you want here for the background
+	glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+
+	//Describe the shape by its vertices
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	//Create an ID to be given at object generation
+	unsigned int vbo;
+	//Pass how many buffers should be created and the reference of the ID to get the value set
+	glGenBuffers(1, &vbo);
+
+	string vs = LoadShader("simpleVertex.shader");
+	const char* vertexShaderSource = vs.c_str();
+
+	string fs = LoadShader("blinkFragment.shader");
+	const char* fragmentShaderSource = fs.c_str();
+
+	//Create identifiers for shaders and give source
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	//now attach shaders to use to the program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//and link it 
+	glLinkProgram(shaderProgram);
+	//now that the program is complete, we can use it 
+	glUseProgram(shaderProgram);
+
+	////////////////VAO////////////////////
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+
+
+	bool isRunning = true;
+	while (isRunning) {
+		// Inputs
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			default:
+				break;
+			}
+		}
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+
+		float speed = 50.0f;
+		float timeValue = (float)SDL_GetTicks() / 1000;
+		float redColor = (sin(timeValue * speed) / 2.0f) + 0.5f;
+		float greenColor = (sin(timeValue * speed + 2) / 2.0f) + 0.5f;
+		float blueColor = (sin(timeValue * speed + 4) / 2.0f) + 0.5f;
+
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "redShift");
+		glUseProgram(shaderProgram);
+		glUniform4f(vertexColorLocation, redColor, greenColor, blueColor, 1.0f);
+		//Draw stuff
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		SDL_GL_SwapWindow(Window); // Swapbuffer
+	}
+	// Quit
+	SDL_DestroyWindow(Window);
+	SDL_GL_DeleteContext(Context);
+
+
 	cin.get();
 	return 0;
+}
+
+string LoadShader(string fileName) {
+	ifstream myFile;
+	myFile.open(fileName);
+	if (myFile.fail()) {
+		cerr << "Error - failed to open " << fileName << endl;
+	}
+	string fileText = "";
+	string line = "";
+	while (getline(myFile, line)) {
+		fileText += line + '\n';
+	}
+
+	myFile.close();
+	return fileText;
 }
